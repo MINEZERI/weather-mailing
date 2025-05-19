e271ef61dbaf80752d66a5dc45636b319e7203b1
Даник <pistriuga.danylo@lll.kpi.ua>
2025-05-19 14:15:48 +0300

    Add subscription service for subscriptions logic

---
+++ src/services/SubscriptionService.ts
@@ -0,0 +1,64 @@
+import validator from "validator";
+import { generateToken, verifyToken } from "../utils/token.js";
+import { SubscriptionRepository } from "../repositories/SubscriptionRepository.js";
+import { EmailService } from "./EmailService.js";
+
+export class SubscriptionService {
+  constructor(
+    private subscriptionRepo: SubscriptionRepository,
+    private emailService: EmailService,
+  ) {}
+
+  private async getSubscriptionEmail(token: string) {
+    let email: string;
+
+    try {
+      const payload = verifyToken(token);
+      email = payload.email;
+    } catch (err) {
+      throw new Error("Invalid token");
+    }
+
+    const existing = await this.subscriptionRepo.findByEmail(email);
+    if (!existing) throw new Error("Subscription not found");
+
+    return email;
+  }
+
+  async subscribe(email: string, city: string, frequency: "hourly" | "daily") {
+    if (!validator.isEmail(email)) {
+      throw new Error("Invalid email");
+    }
+    if (!validator.matches(city, /^[a-zA-Z\s\-]+$/)) {
+      throw new Error("Invalid city");
+    }
+
+    const existing = await this.subscriptionRepo.findByEmail(email);
+    if (existing) {
+      throw new Error("Email already subscribed"); // 409
+    }
+
+    await this.subscriptionRepo.create({
+      email,
+      city,
+      frequency,
+    });
+
+    const token = generateToken({ email, city, frequency });
+
+    await this.emailService.sendConfirmation(email, token);
+
+    return { message: "Confirmation email sent" };
+  }
+
+  async confirm(token: string) {
+    const email = await this.getSubscriptionEmail(token);
+
+    await this.subscriptionRepo.confirmByEmail(email);
+  }
+  async unsubscribe(token: string) {
+    const email = await this.getSubscriptionEmail(token);
+
+    await this.subscriptionRepo.deleteByEmail(email);
+  }
+}
